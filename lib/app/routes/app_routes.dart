72bcfdfa59b1e053a80f5314d4a5e83f5e873bd5
GhadaBadr <91428281+GhadaBadr@users.noreply.github.com>
2022-01-03 23:23:11 +0200

    Add files via upload

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: constant_identifier_names
+
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+
+  static const HOME = _Paths.HOME;
+  static const DEALERSHIPS = _Paths.DEALERSHIPS;
+  static const BODYTYPE_DETAILS = _Paths.BODYTYPE_DETAILS;
+  static const BRAND_DETAILS = _Paths.BRAND_DETAILS;
+  static const NEW_AGENCY_DETAILS = _Paths.NEW_AGENCY_DETAILS;
+  static const VIDEO_DETAILS = _Paths.VIDEO_DETAILS;
+  static const NEW_AGENCY_ITEM_DETAILS = _Paths.NEW_AGENCY_ITEM_DETAILS;
+  static const BODYTYPE_DETAILS_DESCRIPTION =
+      _Paths.BODYTYPE_DETAILS_DESCRIPTION;
+  static const BRAND_DETAILS_DESCRIPTION = _Paths.BRAND_DETAILS_DESCRIPTION;
+  static const PRICING = _Paths.PRICING;
+  static const SING_IN = _Paths.SING_IN;
+  static const NEW_USER = _Paths.NEW_USER;
+  static const ACTIVATION = _Paths.ACTIVATION;
+  static const SERVICES = _Paths.SERVICES;
+  static const SERVICES_GRID_ITEM_DETAILS = _Paths.SERVICES_GRID_ITEM_DETAILS;
+  static const GRID_ITEM_DESCRPTIONS_DETAILS =
+      _Paths.GRID_ITEM_DESCRPTIONS_DETAILS;
+  static const STORE = _Paths.STORE;
+  static const INSURANCE = _Paths.INSURANCE;
+  static const STORE_ALL_PRODUCTS = _Paths.STORE_ALL_PRODUCTS;
+  static const CART = _Paths.CART;
+  static const STORE_PRODUCT_DETAILS = _Paths.STORE_PRODUCT_DETAILS;
+  static const SEARCH = _Paths.SEARCH;
+}
+
+abstract class _Paths {
+  static const HOME = '/home';
+  static const DEALERSHIPS = '/dealerships';
+  static const BODYTYPE_DETAILS = '/bodytype-details';
+  static const BRAND_DETAILS = '/brand-details';
+  static const NEW_AGENCY_DETAILS = '/new-agency-details';
+  static const VIDEO_DETAILS = '/video-details';
+  static const NEW_AGENCY_ITEM_DETAILS = '/new-agency-item-details';
+  static const BODYTYPE_DETAILS_DESCRIPTION = '/bodytype-details-description';
+  static const BRAND_DETAILS_DESCRIPTION = '/brand-details-description';
+  static const PRICING = '/pricing';
+  static const SING_IN = '/sing-in';
+  static const NEW_USER = '/new-user';
+  static const ACTIVATION = '/activation';
+  static const SERVICES = '/services';
+  static const SERVICES_GRID_ITEM_DETAILS = '/services-grid-item-details';
+  static const GRID_ITEM_DESCRPTIONS_DETAILS = '/grid-item-descrptions-details';
+  static const STORE = '/store';
+  static const INSURANCE = '/insurance';
+  static const STORE_ALL_PRODUCTS = '/store-all-products';
+  static const CART = '/cart';
+  static const STORE_PRODUCT_DETAILS = '/store-product-details';
+  static const SEARCH = '/search';
+}
